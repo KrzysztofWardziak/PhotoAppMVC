ca02b3bad2db13f96a14e3ec61afe1a1143ff52e
Krzysztof <krzysztof.wardziak97@gmail.com>
2020-10-01 12:44:04 +0200

    Added Dependency Injection

---
+++ PhotoAppMVC.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PhotoAppMVC.Domain.Interface;
+using PhotoAppMVC.Infrastructure.Repositores;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoAppMVC.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IItemRepository, ItemRepository>();
+            return services;
+        }
+    }
+}
