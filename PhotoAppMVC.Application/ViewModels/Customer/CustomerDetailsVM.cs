7407542d15ae4fbe4a2225e45752fed9db5cd563
Krzysztof <krzysztof.wardziak97@gmail.com>
2020-10-01 12:21:04 +0200

    Added Automapper

--- PhotoAppMVC.Application/ViewModels/Customer/CustomerDetailsVM.cs
+++ PhotoAppMVC.Application/ViewModels/Customer/CustomerDetailsVM.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using AutoMapper;
+using PhotoAppMVC.Application.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PhotoAppMVC.Application.ViewModels.Customer
 {
-    public class CustomerDetailsVM
+    public class CustomerDetailsVM : IMapFrom<PhotoAppMVC.Domain.Model.Customer>
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string NIP { get; set; }
         public string REGON { get; set; }
         public string CEOFullName { get; set; }
@@ -15,5 +18,17 @@ namespace PhotoAppMVC.Application.ViewModels.Customer
         public List<ContactDetailListVM> Emails { get; set; }
         public List<ContactDetailListVM> PhoneNumber { get; set; }
 
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<PhotoAppMVC.Domain.Model.Customer, CustomerDetailsVM>()
+               .ForMember(s => s.CEOFullName, opt => opt.MapFrom(d => d.CEOName + " " + d.CEOLastName))
+               .ForMember(s => s.FirstLineOfContactInformation, 
+               opt => opt.MapFrom(d => d.CustomerContactInformation.FirstName + " " + d.CustomerContactInformation.LastName))
+               .ForMember(s => s.Addresses, opt => opt.Ignore())
+               .ForMember(s => s.Emails, opt=> opt.Ignore())
+               .ForMember(s => s.PhoneNumber, opt => opt.Ignore());
+
+        }
     }
 }
--- PhotoAppMVC.Application/ViewModels/Customer/CustomerForListVM.cs
+++ PhotoAppMVC.Application/ViewModels/Customer/CustomerForListVM.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using AutoMapper;
+using PhotoAppMVC.Application.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PhotoAppMVC.Application.ViewModels.Customer
 {
-    public class CustomerForListVM
+    public class CustomerForListVM : IMapFrom<PhotoAppMVC.Domain.Model.Customer>
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string NIP { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<PhotoAppMVC.Domain.Model.Customer, CustomerForListVM>();
+
+        }
 
     }
 }
